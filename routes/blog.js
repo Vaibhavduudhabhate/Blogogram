f297df56c55721680c069af1ddb77dbaebb6d647
kunal dudhabhate <kunaldudhabhate@gmail.com>
2024-09-28 01:06:55 +0530

    blog page setup

--- routes/blog.js
+++ routes/blog.js
@@ -22,6 +22,14 @@ router.get('/add-new',(req,res)=>{
     })
 })
 
+router.get("/:id",async(req,res)=>{
+    const blog = await Blog.findById(req.params.id);
+    return res.render("blog",{
+        user : req.user,
+        blog,
+    })
+})
+
 router.post('/',upload.single('coverImage'),async(req,res)=>{
     const {title ,body} = req.body;
    const blog = await Blog.create({
